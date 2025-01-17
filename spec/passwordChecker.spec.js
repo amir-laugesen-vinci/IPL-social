164285a2d4425b82a61baf8b091c11425fb7f9a7
Laugesen <amir.laugesen@student.vinci.be>
2025-01-17 16:39:07 +0100

    add: test for existing password but less than 8

--- spec/passwordChecker.spec.js
+++ spec/passwordChecker.spec.js
@@ -8,4 +8,10 @@ describe("it should verify all steps of the password to meet expectation for the
         const result = main.passwordChecker("");
         expect(result).toBe(false);
     });
+
+    it("should fail if password is less than 8 characters", function() {
+        
+        const result = main.passwordChecker("abc123");
+        expect(result).toBe(false);
+    });
 });
