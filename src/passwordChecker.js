d158cdc94e48ac8c52fa8e33dc6ab2423455363a
Laugesen <amir.laugesen@student.vinci.be>
2025-01-17 16:47:19 +0100

    add: new main function to verify if there is ipl in the password

--- src/passwordChecker.js
+++ src/passwordChecker.js
@@ -3,6 +3,7 @@ export class Main {
         const hasMinLength = password.length >= 8;
         const hasSpecialChar = /[!@#$%^&*(),.?":{}|<>]/.test(password);
         const hasDigit = /\d/.test(password);
-        return hasMinLength && hasSpecialChar && hasDigit;
+        const noIPL = !/ipl/i.test(password);
+        return hasMinLength && hasSpecialChar && hasDigit && noIPL;
     }
 }
